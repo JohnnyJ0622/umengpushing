afd697b470a5d82d1b6d1c249b27d7e357941a30
JohnnyJ0622 <johnnyj0622@yahoo.com>
2017-06-30 22:24:16 +1200

    initialize.

---
+++ SlatPlusPushing/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace SlatPlusPushing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.articleIDTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.articleTitleTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.articleContentRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.AndroidCheckBox = new System.Windows.Forms.CheckBox();
+            this.iOSCheckBox = new System.Windows.Forms.CheckBox();
+            this.pushButton = new System.Windows.Forms.Button();
+            this.buttonPull = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // articleIDTextBox
+            // 
+            this.articleIDTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.articleIDTextBox.Location = new System.Drawing.Point(209, 46);
+            this.articleIDTextBox.Name = "articleIDTextBox";
+            this.articleIDTextBox.Size = new System.Drawing.Size(327, 26);
+            this.articleIDTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Article ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Article Title:";
+            // 
+            // articleTitleTextBox
+            // 
+            this.articleTitleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.articleTitleTextBox.Location = new System.Drawing.Point(209, 101);
+            this.articleTitleTextBox.Name = "articleTitleTextBox";
+            this.articleTitleTextBox.Size = new System.Drawing.Size(434, 26);
+            this.articleTitleTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(66, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Article Content:";
+            // 
+            // articleContentRichTextBox
+            // 
+            this.articleContentRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.articleContentRichTextBox.Location = new System.Drawing.Point(209, 157);
+            this.articleContentRichTextBox.Name = "articleContentRichTextBox";
+            this.articleContentRichTextBox.Size = new System.Drawing.Size(434, 96);
+            this.articleContentRichTextBox.TabIndex = 6;
+            this.articleContentRichTextBox.Text = "";
+            // 
+            // AndroidCheckBox
+            // 
+            this.AndroidCheckBox.AutoSize = true;
+            this.AndroidCheckBox.Checked = true;
+            this.AndroidCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AndroidCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AndroidCheckBox.Location = new System.Drawing.Point(70, 306);
+            this.AndroidCheckBox.Name = "AndroidCheckBox";
+            this.AndroidCheckBox.Size = new System.Drawing.Size(83, 24);
+            this.AndroidCheckBox.TabIndex = 7;
+            this.AndroidCheckBox.Text = "Android";
+            this.AndroidCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // iOSCheckBox
+            // 
+            this.iOSCheckBox.AutoSize = true;
+            this.iOSCheckBox.Checked = true;
+            this.iOSCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.iOSCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iOSCheckBox.Location = new System.Drawing.Point(209, 306);
+            this.iOSCheckBox.Name = "iOSCheckBox";
+            this.iOSCheckBox.Size = new System.Drawing.Size(54, 24);
+            this.iOSCheckBox.TabIndex = 8;
+            this.iOSCheckBox.Text = "iOS";
+            this.iOSCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // pushButton
+            // 
+            this.pushButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pushButton.Location = new System.Drawing.Point(568, 298);
+            this.pushButton.Name = "pushButton";
+            this.pushButton.Size = new System.Drawing.Size(75, 32);
+            this.pushButton.TabIndex = 9;
+            this.pushButton.Text = "push";
+            this.pushButton.UseVisualStyleBackColor = true;
+            this.pushButton.Click += new System.EventHandler(this.pushButton_Click);
+            // 
+            // buttonPull
+            // 
+            this.buttonPull.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPull.Location = new System.Drawing.Point(568, 46);
+            this.buttonPull.Name = "buttonPull";
+            this.buttonPull.Size = new System.Drawing.Size(75, 26);
+            this.buttonPull.TabIndex = 10;
+            this.buttonPull.Text = "Pull";
+            this.buttonPull.UseVisualStyleBackColor = true;
+            this.buttonPull.Click += new System.EventHandler(this.buttonPull_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 385);
+            this.Controls.Add(this.buttonPull);
+            this.Controls.Add(this.pushButton);
+            this.Controls.Add(this.iOSCheckBox);
+            this.Controls.Add(this.AndroidCheckBox);
+            this.Controls.Add(this.articleContentRichTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.articleTitleTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.articleIDTextBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SLATPLUS MESSAGE PUSHING";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox articleIDTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox articleTitleTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox articleContentRichTextBox;
+        private System.Windows.Forms.CheckBox AndroidCheckBox;
+        private System.Windows.Forms.CheckBox iOSCheckBox;
+        private System.Windows.Forms.Button pushButton;
+        private System.Windows.Forms.Button buttonPull;
+    }
+}
+
---
+++ SlatPlusPushing/Form1.cs
@@ -0,0 +1,257 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlatPlusPushing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string getPostBody(string deviceType)
+        {
+            var paramAndroid = new
+            {
+                appkey = "xxxxxxxxxxxxxxxxxxxxxxx",
+                timestamp = ConvertDateTimeToInt(DateTime.Now.AddHours(1)).ToString().Substring(0, 10),
+                production_mode = "true",
+                description = "文章推送ID: " + articleIDTextBox.Text,
+                type = "broadcast",
+                payload = new
+                {
+                    extra = new
+                    {
+                        type = "article",
+                        id = articleIDTextBox.Text,
+                    },
+                    body = new
+                    {
+                        title = articleTitleTextBox.Text,
+                        ticker = articleTitleTextBox.Text,
+                        text = articleContentRichTextBox.Text,
+                        after_open = "go_app",
+                        play_vibrate = "true",
+                        play_sound = "true",
+                        play_lights = "true"
+                    },
+                    display_type = "notification"
+                }
+            };
+
+            var paramIOS = new
+            {
+                appkey = "xxxxxxxxxxxxxxxxxxxxxxx",
+                timestamp = ConvertDateTimeToInt(DateTime.Now.AddHours(1)).ToString().Substring(0, 10),
+                production_mode = "true",
+                description = "走进新西兰",
+                type = "broadcast",
+                payload = new
+                {
+                    aps = new
+                    {
+                        sound = "default",
+                        badge = 1,
+                        alert = new
+                        {
+                            body = articleContentRichTextBox.Text,
+                            title = articleTitleTextBox.Text
+                        },
+                    },
+                    articleId = articleIDTextBox.Text,
+                    category = "新西兰",
+                    policy = new
+                    {
+                        expire_time = DateTime.Now.AddDays(3).ToString("yyyy-MM-dd HH:mm:ss")
+                    }
+                }
+            };
+
+            var serializer = JsonSerializer.Create();
+            var jsonWriter = new System.IO.StringWriter();
+            if ("Andriod".Equals(deviceType))
+            {
+                serializer.Serialize(jsonWriter, paramAndroid);
+            }
+            else
+            {
+                serializer.Serialize(jsonWriter, paramIOS);
+            }
+            var post_body = jsonWriter.ToString();
+            post_body = unicode_js_0(post_body);//.Replace(":", ": ").Replace(",", ", ");
+            var str = post_body;
+            return str;
+        }
+
+        private string GetSign(string deviceType)
+        {
+            var app_master_secret = "xxxxxxxxxxxxxxxxxxxxxxx";
+            if ("Andriod".Equals(deviceType))
+            {
+                app_master_secret = "xxxxxxxxxxxxxxxxxxxxxxx";
+            }
+
+            if ("iOS".Equals(deviceType))
+            {
+                app_master_secret = "xxxxxxxxxxxxxxxxxxxxxxx";
+            }
+
+            var method = "POST";
+            var url = "http://msg.umeng.com/api/send";
+            string postBody = this.getPostBody(deviceType);
+            var str = method + url + postBody + app_master_secret;
+            var sign = MD5(str);
+            return sign;
+        }
+
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private string MD5(string s)
+        {
+
+            var x = new MD5CryptoServiceProvider();
+            var bs = System.Text.Encoding.UTF8.GetBytes(s);
+            bs = x.ComputeHash(bs);
+            var str = new System.Text.StringBuilder();
+            foreach (byte b in bs)
+            {
+                str.Append(b.ToString("x2").ToLower());
+            }
+            return str.ToString();
+        }
+        /// <summary>
+        /// 中文转unicode（符合js规则的）
+        /// </summary>
+        /// <returns></returns>
+        public string unicode_js_0(string str)
+        {
+            var outStr = string.Empty;
+            if (!string.IsNullOrEmpty(str))
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (Regex.IsMatch(str[i].ToString(), @"[\u4e00-\u9fa5]")) { outStr += "\\u" + ((int)str[i]).ToString("x"); }
+                    else { outStr += str[i]; }
+                }
+            }
+            return outStr;
+        }
+
+        private void pushButton_Click(object sender, EventArgs e)
+        {
+            pushButton.Enabled = false;
+
+            string message = "";
+            if ("".Equals(articleIDTextBox.Text))
+            {
+                MessageBox.Show("The article ID cannot be empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                articleIDTextBox.Focus();
+
+            }
+            else if ("".Equals(articleTitleTextBox.Text))
+            {
+                MessageBox.Show("The article title cannot be empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                articleTitleTextBox.Focus();
+            }
+            else if ("".Equals(articleContentRichTextBox.Text))
+            {
+                MessageBox.Show("The article content cannot be empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                articleContentRichTextBox.Focus();
+            }
+            else
+            {
+                if (iOSCheckBox.Checked)
+                {
+                    message = message + this.callUMengAPI("iOS");
+                }
+                if (AndroidCheckBox.Checked)
+                {
+                    message = message + this.callUMengAPI("Andriod");
+                }
+                MessageBox.Show(message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            pushButton.Enabled = true;
+        }
+
+        private string callUMengAPI(string device)
+        {
+            string message = device;
+            string sign = this.GetSign(device);
+            string url = "http://msg.umeng.com/api/send?sign=" + sign;
+
+            Console.WriteLine(url);
+
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            string data = getPostBody(device);
+
+            Console.WriteLine(data);
+
+            byte[] byteData = UTF8Encoding.UTF8.GetBytes(data.ToString());
+            request.ContentLength = byteData.Length;
+            try
+            {
+                using (Stream postStream = request.GetRequestStream())
+                {
+                    postStream.Write(byteData, 0, byteData.Length);
+                }
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+                message = message + " pushing succeed! ";
+                return message;
+            }
+            catch (Exception ex)
+            {
+                message = message + " pushing FAIL! ";
+                return message;
+            }
+        }
+
+        private long ConvertDateTimeToInt(System.DateTime time)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1, 0, 0, 0, 0));
+            long t = (time.Ticks - startTime.Ticks) / 10000;   //除10000调整为13位      
+            return t;
+        }
+
+        private void buttonPull_Click(object sender, EventArgs e)
+        {
+            buttonPull.Enabled = false;
+            pushButton.Enabled = false;
+
+            WebClient wc = new WebClient();
+            wc.Credentials = CredentialCache.DefaultCredentials;
+            Byte[] pageData = wc.DownloadData("https://app.nzinfo.cn/index.php/h5/article/detail?id=" + articleIDTextBox.Text);
+            String webString = Encoding.UTF8.GetString(pageData);
+            Console.WriteLine(webString);
+
+            Match TitleMatch = Regex.Match(webString, "<title>([^<]*)</title>", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            articleTitleTextBox.Text = TitleMatch.Groups[1].Value;
+            
+            buttonPull.Enabled = true;
+            pushButton.Enabled = true;
+        }
+    }
+}
